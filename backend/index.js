5fcae80b0091ea9ad75486e36f95e173594bdfc1
chanakyasai <chanakya@infiniticube.in>
2025-05-01 13:09:58 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+// const userRoutes = require("./routes/user.routes");
+const errorHandler = require("./middlewares/errorHandler");
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to DB
+connectDB();
+
+// Routes
+// app.use("/api/users", userRoutes);
+
+// Error Handler
+app.use(errorHandler);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
+});
