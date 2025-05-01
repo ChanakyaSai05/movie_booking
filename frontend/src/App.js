7baf7b1bbd8ea716bafa21cfc5f351383972941d
chanakyasai <chanakya@infiniticube.in>
2025-05-01 13:25:56 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/register"} element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
