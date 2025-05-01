525e00a8a7bf681befd739308d8cb4eab2466cef
chanakyasai <chanakya@infiniticube.in>
2025-05-01 19:04:22 +0530

    created user and admin route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,21 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path={"/login"} element={<Login />} />
-          <Route path={"/register"} element={<Register />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path={"/login"} element={<Login />} />
+            <Route path={"/register"} element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
